09d473cbe9e0466568445ce4e5e3cc3ff32a3897
jaskrrish <jashehz@gmail.com>
2024-09-08 15:35:51 +0530

    feat: Update the Connect Wallet Modal UI

---
+++ src/app/mint/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const MintPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#00041F]">
+      <h1 className="text-4xl font-bold text-white">Mint</h1>
+    </div>
+  );
+};
+
+export default MintPage;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,8 @@
 import { X } from "lucide-react";
 import React from "react";
+import { Work_Sans } from "next/font/google";
+
+const workSans = Work_Sans({ subsets: ["latin"] });
 
 interface ModalProps {
   openModal: boolean;
@@ -15,22 +18,24 @@ const Modal: React.FC<ModalProps> = ({ openModal, onClose, children }) => {
     <div
       onClick={onClose}
       className={`fixed inset-0 flex justify-center items-center transition-colors ${
-        openModal ? "visible bg-black/40" : "invisible"
+        openModal ? "visible bg-black/20" : "invisible"
       } `}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`bg-[#1b1b1b] text-white rounded-xl shadow md:p-6 p-3 transition-all ${
+        className={`bg-[#00041F]/20 backdrop-blur-md text-white rounded-xl z-[100] shadow md:p-8 p-3 transition-all ${
           openModal ? "scale-100 opacity-100" : "scale-125 opacity-0"
         } `}
       >
         <div className="flex items-center justify-center gap-x-4">
-          <h1 className={`md:text-2xl text-lg mb-2 mx-4 font-bold `}>
+          <h1
+            className={`${workSans.className} md:text-2xl text-lg mb-2 mx-4 font-bold`}
+          >
             Connect Your Wallet
           </h1>
           <button
             onClick={CloseModal}
-            className="pb-6 rounded-lg text-gray-400 bg-[#1b1b1b] hover:bg-[#1d1d1d]"
+            className="pb-6 rounded-lg text-gray-400"
           >
             <X />
           </button>
