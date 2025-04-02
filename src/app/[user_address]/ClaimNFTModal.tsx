e3d83fe141b165c1eda536f27b823da627057c35
Saquib Shaikh <shaikhsaquib422@gmail.com>
2025-04-02 12:34:44 +0530

    added the update profile functionality

--- src/app/[user_address]/ClaimNFTModal.tsx
+++ src/app/[user_address]/ClaimNFTModal.tsx
@@ -98,14 +98,14 @@ const NFTModal: React.FC<NFTModalProps> = ({
         {/* Claim Button */}
         <button
           onClick={handleClaimNft}
-          className="flex items-center gap-2 justify-center bg-[#319795] text-white px-4 py-2 rounded-md text-lg font-semibold hover:bg-[#267e6e] transition w-full mt-4"
+          className="flex items-center gap-2 justify-center bg-blue-700 text-white px-4 py-2 rounded-md text-lg font-semibold hover:bg-blue-600 transition w-full mt-4"
         >
           <HandCoins size={20} />
           Claim NFT
         </button>
         <button
           onClick={handleUpdateNftMetadata}
-          className="flex items-center gap-2 justify-center bg-[#319795] text-white px-4 py-2 rounded-md text-lg font-semibold hover:bg-[#267e6e] transition w-full mt-4"
+          className="flex items-center gap-2 justify-center bg-blue-700 text-white px-4 py-2 rounded-md text-lg font-semibold hover:bg-blue-600 transition w-full mt-4"
         >
           <HandCoins size={20} />
           Update NFT Metadata
