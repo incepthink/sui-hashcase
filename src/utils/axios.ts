283d649c54195576a85347ad3adc47eff9cbbf2f
dodaa08 <dodakartik26@gmail.com>
2025-08-22 19:44:34 +0530

    updated the ui

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL:  "http://localhost:8001",
+  baseURL:  "https://api.hashcase.co",
 });
 
 axiosInstance.interceptors.request.use(
