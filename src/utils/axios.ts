66cab6dd0084651d3d1dd78a862d2c5555c9386d
incepthink <incepthink@gmail.com>
2026-03-04 04:51:14 +0530

    changes

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 // TODO
 const axiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV !== "development"
+    process.env.NODE_ENV === "development"
       ? "http://localhost:8000"
       : "https://api.hashcase.co",
 });
